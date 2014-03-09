a46040bd330769adb71cbbdeb44ac71693a9e951
mishyTheBear <hamish.dickson@gmail.com>
2014-03-09 16:10:43 +0000

    Initial commit
    This probably needs a bunch of refactoring

---
+++ src/main/java/co/snowbear/RestTemplate/models/Particle.java
@@ -0,0 +1,72 @@
+package co.snowbear.RestTemplate.models;
+
+/**
+ * Created by hamish dickson on 09/03/2014.
+ *
+ * Bean for particle
+ */
+public class Particle {
+    private String name;
+    private String another;
+    // in keV
+    private int mass;
+
+    public Particle() {
+    }
+
+
+    public Particle(String name, String another, int mass) {
+        this.name = name;
+        this.another = another;
+        this.mass = mass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAnother() {
+        return another;
+    }
+
+    public void setAnother(String another) {
+        this.another = another;
+    }
+
+    public int getMass() {
+        return mass;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    @Override
+    public String toString() {
+        return "Particle{" +
+                "name='" + name + '\'' +
+                ", another='" + another + '\'' +
+                ", mass=" + mass +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Particle particle = (Particle) o;
+
+        return !(name != null ? !name.equals(particle.name) : particle.name != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
